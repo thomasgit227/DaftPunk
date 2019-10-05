67c459bd5eb2b9dab4b35eb4041042674ffe9cb1
Thomas Floyd <tefloyd1215@gmail.com>
2019-10-04 20:03:06 -0500

    -Initial Commit

---
+++ DaftPunk_Thomas/DaftPunk_Thomas.ino
@@ -0,0 +1,104 @@
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+ #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+
+#define EARS_PIN 4
+#define VISOR_PIN 3
+
+#define EARS_COUNT 16
+#define VISOR_COUNT 60
+
+Adafruit_NeoPixel earRings(EARS_COUNT, EARS_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel visorStrip(VISOR_COUNT, VISOR_PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+  clock_prescale_set(clock_div_1);
+#endif
+
+  initialize();       
+  clearStrip(earRings);
+  clearStrip(visorStrip);  
+  Serial.begin(9600);        
+}
+
+void loop() {
+  colorWipe(earRings, earRings.Color(255,   0,   0), 50); // Red
+  colorWipe(earRings, earRings.Color(  0, 255,   0), 50); // Green
+  colorWipe(earRings, earRings.Color(  0,   0, 255), 50); // Blue
+//
+//  theaterChase(strip.Color(127, 127, 127), 50); // White, half brightness
+//  theaterChase(strip.Color(127,   0,   0), 50); // Red, half brightness
+//  theaterChase(strip.Color(  0,   0, 127), 50); // Blue, half brightness
+//
+//  rainbow(earRings, 10);             // Flowing rainbow cycle along the whole strip
+//  theaterChaseRainbow(50); // Rainbow-enhanced theaterChase variant
+}
+
+
+
+void colorWipe(Adafruit_NeoPixel strip, uint32_t color, int wait) {
+  for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
+    strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
+    strip.show();                          //  Update strip to match
+    delay(wait);                           //  Pause for a moment
+  }
+}
+
+void theaterChase(Adafruit_NeoPixel strip, uint32_t color, int wait) {
+  for(int a=0; a<10; a++) {  // Repeat 10 times...
+    for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
+      strip.clear();         //   Set all pixels in RAM to 0 (off)
+      // 'c' counts up from 'b' to end of strip in steps of 3...
+      for(int c=b; c<strip.numPixels(); c += 3) {
+        strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
+      }
+      strip.show(); // Update strip with new contents
+      delay(wait);  // Pause for a moment
+    }
+  }
+}
+
+void rainbow(Adafruit_NeoPixel strip,int wait) {
+  for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
+    for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
+      int pixelHue = firstPixelHue + (i * 65536L / strip.numPixels());
+      strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue)));
+    }
+    strip.show(); // Update strip with new contents
+    delay(wait);  // Pause for a moment
+  }
+}
+
+void theaterChaseRainbow(Adafruit_NeoPixel strip, int wait) {
+  int firstPixelHue = 0;     // First pixel starts at red (hue 0)
+  for(int a=0; a<30; a++) {  // Repeat 30 times...
+    for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
+      strip.clear();         //   Set all pixels in RAM to 0 (off)
+      // 'c' counts up from 'b' to end of strip in increments of 3...
+      for(int c=b; c<strip.numPixels(); c += 3) {
+        // hue of pixel 'c' is offset by an amount to make one full
+        // revolution of the color wheel (range 65536) along the length
+        // of the strip (strip.numPixels() steps):
+        int      hue   = firstPixelHue + c * 65536L / strip.numPixels();
+        uint32_t color = strip.gamma32(strip.ColorHSV(hue)); // hue -> RGB
+        strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
+      }
+      strip.show();                // Update strip with new contents
+      delay(wait);                 // Pause for a moment
+      firstPixelHue += 65536 / 90; // One cycle of color wheel over 90 frames
+    }
+  }
+}
+
+void initialize() {
+  visorStrip.begin();
+  visorStrip.setBrightness(100);
+  earRings.begin();  
+  earRings.setBrightness(100);
+}
+
+void clearStrip(Adafruit_NeoPixel strip) {
+  strip.show();
+}
