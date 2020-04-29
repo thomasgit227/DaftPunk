2f9adf6371832b6dbe9b330099ac6e6c3472f6a4
Thomas Floyd <tefloyd1215@gmail.com>
2020-04-29 01:39:01 -0500

    -Pseudo-Threaded Both Strips -Started Making Visor Sequences

--- DaftPunk_Thomas/DaftPunk_Thomas.ino
+++ DaftPunk_Thomas/DaftPunk_Thomas.ino
@@ -3,16 +3,19 @@
  #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
 #endif
 
-#define EARS_PIN 4
-#define VISOR_PIN 3
+#define EARS_PIN 6
+#define VISOR_PIN 7
 
 #define EARS_COUNT 16
-#define VISOR_COUNT 60
+#define VISOR_COUNT 48
 
 Adafruit_NeoPixel earRings(EARS_COUNT, EARS_PIN, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel visorStrip(VISOR_COUNT, VISOR_PIN, NEO_GRB + NEO_KHZ800);
 
+long firstPixelHue = 0;
+
 void setup() {
+  
 #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
   clock_prescale_set(clock_div_1);
 #endif
@@ -24,16 +27,11 @@ void setup() {
 }
 
 void loop() {
-//  colorWipe(visorStrip, visorStrip.Color(255,   0,   0), 10); // Red
-//  colorWipe(visorStrip, visorStrip.Color(  0, 255,   0), 10); // Green
-//  colorWipe(visorStrip, visorStrip.Color(  0,   0, 255), 10); // Blue
-  
-//  theaterChase(visorStrip, visorStrip.Color(0, 127, 0), 50); // White, half brightness
-//  theaterChase(visorStrip, visorStrip.Color(127,   0,   0), 50); // Red, half brightness
-//  theaterChase(visorStrip, visorStrip.Color(  0,   0, 127), 50); // Blue, half brightness
 
-//  rainbow(visorStrip, 10);             // Flowing rainbow cycle along the whole strip
-//  theaterChaseRainbow(visorStrip, 50); // Rainbow-enhanced theaterChase variants
+  visorStrip.begin();
+  
+  visorSequence(visorStrip);
+
 }
 
 
@@ -65,50 +63,56 @@ void theaterChase(Adafruit_NeoPixel strip, uint32_t color, int wait) {
   }
 }
 
-// Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
-void rainbow(Adafruit_NeoPixel strip, int wait) {
-  // Hue of first pixel runs 5 complete loops through the color wheel.
-  // Color wheel has a range of 65536 but it's OK if we roll over, so
-  // just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
-  // means we'll make 5*65536/256 = 1280 passes through this outer loop:
-  for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
-    for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
-      // Offset pixel hue by an amount to make one full revolution of the
-      // color wheel (range of 65536) along the length of the strip
-      // (strip.numPixels() steps):
-      int pixelHue = firstPixelHue + (i * 65536L / strip.numPixels());
-      // strip.ColorHSV() can take 1 or 3 arguments: a hue (0 to 65535) or
-      // optionally add saturation and value (brightness) (each 0 to 255).
-      // Here we're using just the single-argument hue variant. The result
-      // is passed through strip.gamma32() to provide 'truer' colors
-      // before assigning to each pixel:
-      strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue)));
-    }
-    strip.show(); // Update strip with new contents
-    delay(wait);  // Pause for a moment
-      strip.begin();
+//// Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
+//void rainbow(Adafruit_NeoPixel strip, int wait) {
+//
+//  for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
+//    for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
+//
+//      int pixelHue = firstPixelHue + (i * 65536L / strip.numPixels());
+//
+//      strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue)));
+//    }
+//    strip.show(); // Update strip with new contents
+//    delay(wait);  // Pause for a moment
+//      strip.begin();
+//  }
+//}
+
+void updateRainbow() {
+  if(firstPixelHue < 5*65536) {
+      firstPixelHue += 256;
   }
+  else {
+    firstPixelHue = 0;
+  }
+  
+  for(int i=0; i<earRings.numPixels(); i++) { // For each pixel in strip...
+    int pixelHue = firstPixelHue + (i * 65536L / earRings.numPixels());
+    earRings.setPixelColor(i, earRings.gamma32(earRings.ColorHSV(pixelHue)));
+  }
+  earRings.show(); // Update strip with new contents
+  earRings.begin();
 }
 
-// Rainbow-enhanced theater marquee. Pass delay time (in ms) between frames.
-void theaterChaseRainbow(Adafruit_NeoPixel strip, int wait) {
-  int firstPixelHue = 0;     // First pixel starts at red (hue 0)
-  for(int a=0; a<30; a++) {  // Repeat 30 times...
-    for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
-      strip.clear();         //   Set all pixels in RAM to 0 (off)
-      // 'c' counts up from 'b' to end of strip in increments of 3...
-      for(int c=b; c<strip.numPixels(); c += 3) {
-        // hue of pixel 'c' is offset by an amount to make one full
-        // revolution of the color wheel (range 65536) along the length
-        // of the strip (strip.numPixels() steps):
-        int      hue   = firstPixelHue + c * 65536L / strip.numPixels();
-        uint32_t color = strip.gamma32(strip.ColorHSV(hue)); // hue -> RGB
-        strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
-      }
-      strip.show();                // Update strip with new contents
-      delay(wait);                 // Pause for a moment
-      firstPixelHue += 65536 / 90; // One cycle of color wheel over 90 frames
-    }
-      strip.begin();
+void visorSequence(Adafruit_NeoPixel strip) {
+  for(int i = 0; i < strip.numPixels()/4; i++) {
+    strip.setPixelColor(i, strip.Color(255,10,0)); //Q2
+    strip.setPixelColor(strip.numPixels()-i-1, strip.Color(255,10,0)); //Q4
+    strip.setPixelColor(strip.numPixels()/2-i-1, strip.Color(255,10,0)); //Q1
+    strip.setPixelColor(strip.numPixels()/2+i, strip.Color(255,10,0)); //Q3 
+    updateRainbow();
+    strip.show();                          
+    delay(20);                           
   }
+  for(int i = 0; i < strip.numPixels()/4; i++) {
+    strip.setPixelColor(i, strip.Color(0,0,0)); //Q2
+    strip.setPixelColor(strip.numPixels()-i-1, strip.Color(0,0,0)); //Q4
+    strip.setPixelColor(strip.numPixels()/2-i-1, strip.Color(0,0,0)); //Q1
+    strip.setPixelColor(strip.numPixels()/2+i, strip.Color(0,0,0)); //Q3  
+    updateRainbow();             
+    strip.show();                          
+    delay(20);                           
+  }
+  delay(200);
 }
